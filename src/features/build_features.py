b23a3095f34ad98554efe12239e2045a3f32efec
Karanraj Chauhan <kachau@redhat.com>
2021-09-23 15:09:19 -0400

    Add custom model

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,1 +1,12 @@
 """Feature extraction code."""
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class DummyTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+        pass
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        return X
