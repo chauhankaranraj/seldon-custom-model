4bea726429919c7065e621d195c719cd1693e3bc
Karanraj Chauhan <kachau@redhat.com>
2021-10-08 15:08:32 -0400

    Add custom transformer to pipeline

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,1 +1,11 @@
 """Feature extraction code."""
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class DummyTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self) -> None:
+        super().__init__()
+    def fit(self, X, y=None, **fit_params):
+        return self
+    def transform(self, X, y=None, **fit_params):
+        return X
