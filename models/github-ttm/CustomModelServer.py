7fe72f16144274daca6b50c61dc2ad1fda699aae
Karanraj Chauhan <kachau@redhat.com>
2021-10-08 16:04:50 -0400

    Update model to read from s3
    Add boto3 to requirementes

--- models/github-ttm/CustomModelServer.py
+++ models/github-ttm/CustomModelServer.py
@@ -1,6 +1,8 @@
 """Here goes the prediction code."""
 import os
 import joblib
+import boto3
+from io import BytesIO
 from typing import Iterable, Dict, List, Union
 
 import numpy as np
@@ -16,12 +18,28 @@ class CustomModelServer(object):
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
         print("Initializing")
+        self.s3_resource = boto3.resource(
+            "s3",
+            endpoint_url=os.getenv("S3_ENDPOINT"),
+            aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
+            aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
+        )
 
     def load(self):
         print("Loading model", os.getpid())
         with open('model.joblib', 'rb') as f:
             self.model = joblib.load(f)
         print("Loaded model")
+
+        print("Loading test model", os.getpid())
+        buffer = BytesIO()
+        s3_object = self.s3_resource.Object(
+            os.getenv("S3_BUCKET"),
+            f"ai4ci/github-pr-ttm/model/model.joblib",
+        )
+        s3_object.download_fileobj(buffer)
+        test_model = joblib.load(buffer)
+        print("Loaded test model")
 
     def class_names(self) -> Iterable[str]:
         return [f"my_class_{i}" for i in range(10)]
